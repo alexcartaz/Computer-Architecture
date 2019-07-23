406626787a537e4b3a2abe651b273fdedf290d0f
Alex <alexander.t.carter@gmail.com>
2019-07-22 17:43:45 -0700

    day 1 done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,13 +1,24 @@
 """CPU functionality."""
 
 import sys
+#_opcode_'s
+LDI = 0b10000010 #Load (LD) Immediate (I)
+PRN = 0b01000111 #Print
+HLT = 0b00000001 #Halt
+
+def hex_to_dec(hex):
+    return int(hex,16)
+
+def binary_to_dec(binary):
+    return int(binary,2)
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [None] * 8
+        self.pc = 0
+        self.reg = [None] * 8
 
     def load(self):
         """Load a program into memory."""
@@ -60,6 +71,41 @@ class CPU:
 
         print()
 
+    def read_memory(self):
+        return self.ram[self.pc]
+
+    def write_memory(self, value):
+        self.ram[self.pc] = value
+
+    def increment_pc(self):
+        if self.pc == 7:
+            self.pc = 0
+        else:
+            self.pc += 1
+
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+            command = self.read_memory()
+
+            #case statement
+            if command == LDI:
+                self.increment_pc()
+                reg_address = self.read_memory()
+                self.increment_pc()
+                self.reg[reg_address] = self.read_memory()
+            elif command == PRN:
+                self.increment_pc()
+                print(self.reg[self.read_memory()])
+            elif command == HLT:
+                running = False
+            else:
+                print("Unknown command")
+                print("command: " + str(command))
+                sys.exit(1)
+
+            #iterate pc
+            #I suspect I will have to remove from mem and write from mem to execute
+            #code from files w/ more than 8 lines of code
+            self.increment_pc()
