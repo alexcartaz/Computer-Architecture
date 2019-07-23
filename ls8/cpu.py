153f5950dc3a755f3c3b5ae59cd6e930abd84f25
Alex <alexander.t.carter@gmail.com>
2019-07-23 14:35:11 -0700

    added stack, push, and pop

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,10 +2,6 @@
 
 import sys
 #_opcode_'s
-LDI = bin(0b10000010) #Load (LD) Immediate (I)
-PRN = bin(0b01000111) #Print
-MUL = bin(0b10100010) #Multiply
-HLT = bin(0b00000001) #Halt
 
 def hex_to_dec(hex):
     return int(hex,16)
@@ -17,79 +13,23 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        self.ram = [None] * 8
+        self.ram = [None] * 256
         self.pc = 0
         self.reg = [None] * 8
-        self.program = []
+        self.sp = 256
+        self.operations = {
+            0b10000010: 'self.op_ldi()',
+            0b01000111: 'self.op_prn()',
+            0b10100010: 'self.op_alu("MUL")',
+            0b00000001: 'self.op_hlt()',
+            0b01000101: 'self.op_push()',
+            0b01000110: 'self.op_pop()',
+        }
 
-    def load(self):
-        """Load a program into memory."""
-        f = open("ls8/examples/mult.ls8", "r")
-        start_pc = self.pc
-        for s in f:
-            #print(s[0])
-            #print(s[0].isdigit())
-            if s[0].isdigit():
-                s = '0b' + s[0:8]
-                print(bin(int(s,2)))
-                self.program.append(bin(int(s,2)))
-        self.update_mem_stack()
-        print(self.program)
-
-    def update_mem_stack(self):
-        self.ram = [None] * 8
-        index = self.pc
-        running = True
-        while running == True:
-            #print('self.pc: '  + str(self.pc))
-            if index < len(self.program) and index < self.pc + 8:
-                self.write_memory(index%8, self.program[index])
-                index += 1
-            else:
-                running = False
-        self.trace()
-
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
-
-        if op == "ADD":
-            self.reg[reg_a] = bin(int(self.reg[reg_a],2) + int(self.reg[reg_b],2))
-        elif op == "SUB":
-            self.reg[reg_a] = bin(int(self.reg[reg_a],2) - int(self.reg[reg_b],2))
-        elif op == "MUL":
-            self.reg[reg_a] = bin(int(self.reg[reg_a],2) * int(self.reg[reg_b],2))
-        else:
-            raise Exception("Unsupported ALU operation")
-
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
-        print(self.ram)
-        '''
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
-
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
-
-        print()
-        '''
-        
-    def read_memory(self):
-        #print('read_mem -> self.pc: '  + str(self.pc))
-        #print(self.ram[self.pc%8])
-        #print('self.pc%8: ' + str(self.pc%8))
-        if self.pc%8 == 0 and self.pc != 0:
-            self.update_mem_stack()
-        return self.ram[self.pc%8]
+    def read_memory(self, address=None):
+        if address == None:
+            address = self.pc
+        return self.ram[address]
 
     def write_memory(self, address, value):
         self.ram[address] = value
@@ -97,36 +37,89 @@ class CPU:
     def increment_pc(self):
         self.pc += 1
 
+    def load(self):
+        """Load a program into memory."""
+        f = open("ls8/examples/stack.ls8", "r")
+        for s in f:
+            if s[0].isdigit():
+                s = '0b' + s[0:8]
+                self.write_memory(self.pc,(int(s,2)))
+                self.increment_pc()
+        self.pc = 0
+
+    def alu(self, op, reg_a, reg_b):
+        """ALU operations."""
+
+        if op == "ADD":
+            self.reg[reg_a] = self.reg[reg_a] + self.reg[reg_b]
+        elif op == "SUB":
+            self.reg[reg_a] = self.reg[reg_a] - self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
+        else:
+            #raise Exception("Unsupported ALU operation")
+            print("Unsupported ALU operation: " + op)
+    
+    def trace(self):
+        """
+        Handy function to print out the CPU state. You might want to call this
+        from run() if you need help debugging.
+        """
+        print(self.ram)
+
     def run(self):
         """Run the CPU."""
-        running = True
-        while running:
-            command = self.read_memory()
-
-            #case statement
-            if command == LDI:
-                self.increment_pc()
-                reg_address = int(self.read_memory(),2)
-                self.increment_pc()
-                self.reg[reg_address] = self.read_memory()
-            elif command == PRN:
-                self.increment_pc()
-                print(int(self.reg[int(self.read_memory(),2)],2))
-            elif command == MUL:
-                op = "MUL"
-                self.increment_pc()
-                reg_a = int(self.read_memory(),2)
-                self.increment_pc()
-                reg_b = int(self.read_memory(),2)
-                self.alu(op,reg_a,reg_b)
-            elif command == HLT:
-                running = False
-            else:
-                print("Unknown command")
-                print("command: " + str(command))
+        while True:
+            op_id = self.read_memory()
+            if op_id not in self.operations:
+                self.op_unknown(op_id)
                 sys.exit(1)
-
-            #iterate pc
-            #I suspect I will have to remove from mem and write from mem to execute
-            #code from files w/ more than 8 lines of code
+            else:
+                eval(self.operations.get(op_id))
             self.increment_pc()
+
+    def op_ldi(self):
+        print('LDI')
+        self.increment_pc()
+        reg_address = self.read_memory()
+        self.increment_pc()
+        self.reg[reg_address] = self.read_memory()
+
+    def op_prn(self):
+        print('PRN')
+        self.increment_pc()
+        #print('prn reg[x]: ' + str(self.read_memory()))
+        print(self.reg[self.read_memory()])
+
+    def op_alu(self, op):
+        print('MUL')
+        self.increment_pc()
+        reg_a = self.read_memory()
+        self.increment_pc()
+        reg_b = self.read_memory()
+        self.alu(op,reg_a,reg_b)
+
+    def op_push(self):
+        print('PUSH')
+        #may need to add a check to make sure sp is not < pc
+        self.increment_pc()
+        self.sp-=1
+        self.write_memory(self.sp, self.reg[self.read_memory()])
+
+    def op_pop(self):
+        print('POP')
+        self.increment_pc()
+        self.reg[self.read_memory()] = self.read_memory(self.sp)
+        self.write_memory(self.sp, None)
+        self.sp+=1
+
+    def op_hlt(self):
+        print('HLT')
+        sys.exit(1)
+        #running = False
+
+    def op_unknown(self, op_id):
+        print("Unknown command")
+        print("command: " + str(command))
+        sys.exit(1)
+            
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -11,4 +11,14 @@ cpu.load()
 #cpu.trace()
 cpu.run()
 
+'''
+LDI = 0b01000111
 
+dict = {
+  130: 'LDI',
+  131: 'b',
+  71: 'PRN'
+}
+
+print(dict[LDI])
+'''
